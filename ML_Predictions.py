e493cfebb9863e1d6083c695b50033c8c34daecf
Michaeldo2004 <123520368+Michaeldo2004@users.noreply.github.com>
2024-02-01 15:08:25 -0800

    Create ML_Predictions.py

---
+++ ML_Predictions.py
@@ -0,0 +1,13 @@
+import pandas as pd
+# ML for prediction
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import train_test_split
+
+class predict():
+
+    def __init__(self, historical_data):
+        self.historical_data = historical_data
+
+    def predictOpening():
+        
+    def predictClosing():
