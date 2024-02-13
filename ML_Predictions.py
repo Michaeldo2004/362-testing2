44cbd35683873588eff81f7e7a3d6bd2306cb1f2
Michaeldo2004 <123520368+Michaeldo2004@users.noreply.github.com>
2024-02-12 20:42:09 -0800

    Update ML_Predictions.py

--- ML_Predictions.py
+++ ML_Predictions.py
@@ -8,6 +8,19 @@ class predict():
     def __init__(self, historical_data):
         self.historical_data = historical_data
 
-    def predictOpening():
-        
-    def predictClosing():
+    # Does the prediction 
+    # Returns: Predicted Price based on the days
+    def predict(self, data, days):
+        return
+    
+    # Calls the predict function on data of only Opening Prices
+    # Returns: Predicted Opening Price based on the days
+    def predictOpening(self, days):
+        OpenData = self.historical_data['Open']
+        return self.predict(OpenData, days)
+
+    # Calls the predict function on data of only Closing Prices
+    # Returns: Predicted Closing Price based on the days
+    def predictClosing(self, days):
+        ClosingData = self.historical_data['Close']
+        return self.predict(ClosingData, days)
