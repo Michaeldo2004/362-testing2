671b7fbbfa09cf9bd263f75743d2d587e0b16192
Michaeldo2004 <123520368+Michaeldo2004@users.noreply.github.com>
2024-03-14 14:50:36 -0700

    Update ML_Predictions.py

--- ML_Predictions.py
+++ ML_Predictions.py
@@ -8,38 +8,38 @@ from sklearn.linear_model import train_test_split
 
 class predict():
 
-    def __init__(self, historical_data):
-        self.historical_data = historical_data
+  def __init__(self, historical_data):
+    self.historical_data = historical_data
 
-    # Does the prediction 
-    # Returns: Predicted Price based on the days
-    def predict(self, data, days):
-        # Reshapes the data into one column
-        x = np.arange(len(data)).reshape(-1,1)
-        y = data.values.reshape(-1,1)
-        
-        # Splits the data for training and testing
-        # 30% of the data for training, 70% for testing
-        xTrain, xTest, yTrain, yTest = train_test_split(x,y,test_size = 0.3, random_state = 1)
+  # Does the prediction
+  # Returns: Predicted Price based on the days
+  def predict(self, data, days):
+    # Reshapes the data into one column
+    x = np.arange(len(data)).reshape(-1, 1)
+    y = data.values.reshape(-1, 1)
 
-        # Makes a Linear Regression Model
-        model = LinearRegression()
-        model.fit(xTrain, yTrain)
-        # Uses the model to predict the price on depending on 'days' parameter
-        future = np.arange(len(data), len(data) + days).reshape(-1,1)
-        futurePrice = model.predict(future)
+    # Splits the data for training and testing
+    # 30% of the data for training, 70% for testing
+    xTrain, xTest, yTrain, yTest = train_test_split(x,y, test_size=0.3, random_state=1)
 
-        #returns the future price based on 'days' parameter
-        return futurePrice
-    
-    # Calls the predict function on data of only Opening Prices
-    # Returns: Predicted Opening Price based on the days
-    def predictOpening(self, days):
-        openData = self.historical_data['Open']
-        return self.predict(openData, days)
+    # Makes a Linear Regression Model
+    model = LinearRegression()
+    model.fit(xTrain, yTrain)
+    # Uses the model to predict the price on depending on 'days' parameter
+    future = np.arange(len(data), len(data) + days).reshape(-1, 1)
+    futurePrice = model.predict(future)
 
-    # Calls the predict function on data of only Closing Prices
-    # Returns: Predicted Closing Price based on the days
-    def predictClosing(self, days):
-        closingData = self.historical_data['Close']
-        return self.predict(closingData, days)
+    #returns the future price based on 'days' parameter
+    return futurePrice
+
+  # Calls the predict function on data of only Opening Prices
+  # Returns: Predicted Opening Price based on the days
+  def predictOpening(self, days):
+    openData = self.historical_data['Open']
+    return self.predict(openData, days)
+
+  # Calls the predict function on data of only Closing Prices
+  # Returns: Predicted Closing Price based on the days
+  def predictClosing(self, days):
+    closingData = self.historical_data['Close']
+    return self.predict(closingData, days)
