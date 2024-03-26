d15d45624a0eac6ab5e75433fe1d8339161c9794
Michaeldo2004 <123520368+Michaeldo2004@users.noreply.github.com>
2024-03-26 11:57:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template, jsonify
 import stock as sk
 import ML_Predictions as ml
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 @app.route('/stock_details', methods=['GET'])
 def get_stock_details():
@@ -10,25 +14,26 @@ def get_stock_details():
     days_to_predict = int(request.args.get('days', 2))  # Default to 2 days if days param not provided
     if not stock_symbol:
         return jsonify({'error': 'Stock symbol is required'}), 400
-
     # No need to validate days_to_predict if it's restricted on the client side
-
     stock = sk.Stock(stock_symbol)
     historical_data = stock.historyCall()
     predict_model = ml.predict(historical_data)
     predicted_prices = predict_model.predictClosing(days_to_predict)
     # Reshaping the predicted prices into 2D format
     predicted_prices = predicted_prices.tolist()
-    current_price, shares_outstanding, market_cap = stock.stockDetails()
-
+    stock_count = stock.stockCount()
+    current_price = stock.currentPrice()
+    market_cap = stock.marketCap()
     return jsonify({
         'symbol': stock_symbol,
-        'current_price': current_price,
-        'shares_outstanding': shares_outstanding,
-        'market_cap': market_cap,
-        'predicted_closing_prices_2d': predicted_prices,
-        'days_predicted': days_to_predict
+        'predicted_prices': predicted_prices,
+        'days_predicted': days_to_predict,
+        'stock_count': stock_count,
+        'current_price' : current_price,
+        'market_cap' : market_cap
     })
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
